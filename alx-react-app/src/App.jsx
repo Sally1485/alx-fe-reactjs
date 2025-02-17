3f8db8d91032021f95a28e18c691e366b38f781d
salomey tackie <salomeytackie555@gmail.com>
2025-02-17 15:09:50 +0000

    committing all changes 13.0

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button ok={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
@@ -44,4 +44,3 @@ function App() {
 }
 
 export default App
-nClic
