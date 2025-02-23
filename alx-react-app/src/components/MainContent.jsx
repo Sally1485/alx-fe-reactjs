d2c5f750b7826c254019951bae7883cc8fbda458
salomey tackie <salomeytackie555@gmail.com>
2025-02-23 15:57:11 +0000

    committing all changes 2.0

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const MainContent = () => {
   return(
-<main style={{color: 'blue', fontSize: '10px'}}>
+<main style={{ color: "blue", fontSize: "10px"}}>
   <p>I love to visit New York, Paris, and Tokyo.</p>
 </main>
   );
