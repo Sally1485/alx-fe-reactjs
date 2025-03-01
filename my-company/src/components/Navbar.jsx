dfe7bd38d4e3e32a6376f0526a920e5621c82768
salomey tackie <salomeytackie555@gmail.com>
2025-03-01 17:04:10 +0000

    committing all changes 3.0

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <div>
-<nav style={{ display: 'flex', gap: '20px', padding: '10px' }}>
-    <Link to='/' style={{fontSize: '20px', fontFamily: 'sans-serif', color: 'purple'}}>Home</Link>
-<Link to='/about' style={{fontSize: '20px', fontFamily: 'sans-serif', color: 'purple'}}>About</Link>
-<Link to='/services' style={{fontSize: '20px', fontFamily: 'sans-serif', color: 'purple'}}>Services</Link>
-<Link to='/contacts' style={{fontSize: '20px', fontFamily: 'sans-serif', color: 'purple'}}>Contacts</Link>
+<nav style={{ display:'flex', gap:'20px', padding:'10px' }}>
+    <Link to='/' style={{fontSize:'20px', fontFamily:'sans-serif', color:'purple'}}>Home</Link>
+<Link to='/about' style={{fontSize:'20px', fontFamily:'sans-serif', color:'purple'}}>About</Link>
+<Link to='/services' style={{fontSize:'20px', fontFamily:'sans-serif', color:'purple'}}>Services</Link>
+<Link to='/contacts' style={{fontSize:'20px', fontFamily:'sans-serif', color:'purple'}}>Contacts</Link>
 </nav>
 </div>
 );
