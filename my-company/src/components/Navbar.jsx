a94dadb8a0be9fccbb59e4adb2cd97d018948401
salomey tackie <salomeytackie555@gmail.com>
2025-03-01 17:20:32 +0000

    committing all changes 5.0

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -4,10 +4,10 @@ const Navbar = () => {
     return (
         <div>
 <nav style={{display:'flex', gap:'20px', padding:'10px' }}>
-    <Link to='/' style={{fontSize:'20px', backgroundColor: 'yellowgreen', color:'purple'}}>Home</Link>
-<Link to='/about' style={{fontSize:'20px',backgroundColor: 'yellowgreen', color:'purple'}}>About</Link>
-<Link to='/services' style={{fontSize:'20px', backgroundColor: 'yellowgreen',  color:'purple'}}>Services</Link>
-<Link to='/contacts' style={{fontSize:'20px', backgroundColor: 'yellowgreen',  color:'purple'}}>Contacts</Link>
+    <Link to='/' style={{fontSize:'20px', backgroundColor: 'yellowgreen', justifyContent: 'center', color:'purple'}}>Home</Link>
+<Link to='/about' style={{fontSize:'20px',backgroundColor: 'yellowgreen', justifyContent: 'center', color:'purple'}}>About</Link>
+<Link to='/services' style={{fontSize:'20px', backgroundColor: 'yellowgreen', justifyContent: 'center', color:'purple'}}>Services</Link>
+<Link to='/contacts' style={{fontSize:'20px', backgroundColor: 'yellowgreen', justifyContent: 'center', color:'purple'}}>Contacts</Link>
 </nav>
 </div>
 );
