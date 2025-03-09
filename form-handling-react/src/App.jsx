31eff396330f76bd9b3250d0613bc977e4abce6f
salomey tackie <salomeytackie555@gmail.com>
2025-03-09 20:49:33 +0000

    comitting changes 4.0

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import FormikForm from './components/formikForm.js'
+import RegistrationForm from './components/RegistrationForm.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
     <FormikForm />
+    <RegistrationForm />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+const RegistrationForm = () => {
+    const [formData, setFormData] = useState({ username: '', email: '', password: ''});
+
+    const handleChange = (e) => {
+        const { username, value } = e.target;
+        const { email, value } = e.target;
+        const { password, value } = e.target;
+        setFormData(prevState => ({ ...prevState, [username]: value }));
+        setFormData(prevState => ({ ...prevState, [email]: value }));
+        setFormData(prevState => ({ ...prevState, [password]: value }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+            />
+            <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+            />
+             <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+            />
+            <button type="submit">Submit</button>
+        </form>
+    );
+};
+
+export default RegistrationForm;
