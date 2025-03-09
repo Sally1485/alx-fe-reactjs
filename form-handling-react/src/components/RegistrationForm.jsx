cd9af3d45e5626391b17d2d22c413db9e5dc76bc
salomey tackie <salomeytackie555@gmail.com>
2025-03-09 21:06:35 +0000

    committing changes 7.0

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react';
 
 const RegistrationForm = () => {
-    const [formData, setFormData] = useState({ username: '', email: '', password: ''});
+    const [formData, setFormData] = useState({ 
+        username: '',
+         email: '',
+          password:''
+        });
+const [username, email, password] = formData;
 
     const handleChange = (e) => {
-        const { username, value } = e.target;
-        const { email, value } = e.target;
-        const { password, value } = e.target;
-        setFormData(prevState => ({ ...prevState, [username]: value }));
-        setFormData(prevState => ({ ...prevState, [email]: value }));
-        setFormData(prevState => ({ ...prevState, [password]: value }));
-    };
+        const {name, value } = e.target;
+                setFormData(prevState => ({ ...prevState, [name]: value }));
+           };
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -21,20 +22,20 @@ const RegistrationForm = () => {
         <form onSubmit={handleSubmit}>
             <input
                 type="text"
-                name="name"
-                value={formData.username}
+                name="username"
+                value={username}
                 onChange={handleChange}
             />
             <input
                 type="email"
                 name="email"
-                value={formData.email}
+                value={email}
                 onChange={handleChange}
             />
              <input
                 type="password"
                 name="password"
-                value={formData.password}
+                value={password}
                 onChange={handleChange}
             />
             <button type="submit">Submit</button>
