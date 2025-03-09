b84527dc33ab9a89ad634e2238be4ffccca4aadc
salomey tackie <salomeytackie555@gmail.com>
2025-03-09 08:23:29 +0000

    committing all changes 5.0

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-const UserProfile = (props) => {
+   
+const UserProfile = () => {
+    const { name, age, bio } = useContext(UserContext);
     return(
         <div style={{ border:"1px solid gray", padding:"10px", margin:"10px" }}>
-            <h2 style={{ color:"blue" }}>Name:{props.name}</h2>
-            <p>Age: <span style={{ fontWeight:"bold" }}>{props.age}</span></p>
-            <p>Bio: {props.bio}</p>
+            <h2 style={{ color:"blue" }}>Name:{name}</h2>
+            <p>Age: <span style={{ fontWeight:"bold" }}>{age}</span></p>
+            <p>Bio: {bio}</p>
         </div>
     );
 };
