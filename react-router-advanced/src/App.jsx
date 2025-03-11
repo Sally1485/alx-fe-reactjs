3aa26064f5d0c42c537f2483886215fae1067906
salomey tackie <salomeytackie555@gmail.com>
2025-03-11 16:44:52 +0000

    committing all changes 2.0

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {BrowserRoute as Router, Routes, Route, Link}  from  'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, Link}  from  'react-router-dom';
 import BlogPosts from './BlogPosts';
 function App() {
   
--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,9 +1,13 @@
-import {Link} from  'react-router-dom';
+import {Link, Routes, Route} from  'react-router-dom';
 function Profile() {
   return (
     <div>
         <Link to="profileSettings">ProfileSettings</Link>
         <Link to="profileDetails">ProfileDetails</Link>
+        <Routes>
+          <Route path= '/profileDetails' element={<ProfileDetails/>} />
+          <Route path= '/profileSettings' element={<ProfileSettings/>} />
+        </Routes>
     </div>
   )
 }
