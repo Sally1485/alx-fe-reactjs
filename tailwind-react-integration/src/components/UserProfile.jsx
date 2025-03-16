158b22c72b5432f15a25cf444775565563d2f50f
salomey tackie <salomeytackie555@gmail.com>
2025-03-16 06:07:33 +0000

    committing changes 5,0

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 function UserProfile() {
     return (
       <div className="user-profile bg-gray-100 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg p-2 sm:p-4 md:p-8"  >
-        <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-24 h-24 sm:w-36 sm:h-36 md:w-48 md:h-48 mx-auto"  />
+        <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-12 h-12 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"  />
         <h1  className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4">John Doe</h1>
         <p className="text-gray-600 text-sm sm:text-base md:text-xl"  >Developer at Example Co. Loves to write code and explore new technologies.</p>
       </div>
