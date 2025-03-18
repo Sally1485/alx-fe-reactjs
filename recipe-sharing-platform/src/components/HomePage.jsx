9dee6bd507bff635e336b244b871674162153036
salomey tackie <salomeytackie555@gmail.com>
2025-03-18 15:19:06 +0000

    committing all changes 2.0

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -11,7 +11,7 @@ const RecipeList=()=>{
         .catch((error) => console.error("Error loading recipes:", error));
      },[])
   return (
-    <div className= "text-white p-2 hover:text-blue-500 sm:p-4 md:p-8 max-w-xs sm:max-w-md md:max-w-lg"   >
+    <div className= "text-white p-2 hover:text-blue-500 sm:p-4 md:p-8 max-w-xs sm:max-w-md md:max-w-lg rounded sm:rounded-md md:rounded-lg shadow"    >
     <h1>Recipe List</h1>
     <ul>
         {recipes.map((recipe, index) =>(
