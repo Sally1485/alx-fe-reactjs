bb9206c050535282d2f0d3c72e126d983e391874
salomey tackie <salomeytackie555@gmail.com>
2025-03-28 15:11:37 +0000

    committing all changes 1.0

---
+++ github-user-search/src/components/Form.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <div>
+        <label htmlFor="username">username</label>
+        <input type="text" name="username" id="username" />
+    </div>
+  )
+}
+
+export default Form;
