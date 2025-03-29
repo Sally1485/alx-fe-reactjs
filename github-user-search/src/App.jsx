1729dd6148fc0ea430ba9ec710ff76820f19c543
salomey tackie <salomeytackie555@gmail.com>
2025-03-29 21:49:32 +0000

    committimg all changes 2.0

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Form from './components/form'
 import fetchUserData from './services/githubService'
 import Search from './components/Search'
+import './index.css';
 function App() {
   const [count, setCount] = useState(0)
 
